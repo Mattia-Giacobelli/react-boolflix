1064c3db31d079f5eec81598cbdbe5b8ad94999f
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-11-19 20:12:29 +0100

    Merge array and tv series fetch results

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,25 +3,42 @@ import { useMovies } from "../Contexts/MoviesProvider";
 
 export default function Main() {
 
-    const { searchInput, setSearchInput, searchedMovies, setSearchedMovies } = useMovies()
+    const { searchInput, setSearchInput, searched, setSearched,
+        flags, tvSeries, setTvSeries, movies, setMovies
+    } = useMovies()
 
     const movieApiKey = import.meta.env.VITE_MOVIE_API_KEY
 
+    console.log(flags);
 
 
 
-    function getSearchedItems() {
+    function getMoviesAndTv() {
+
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${movieApiKey}&language=it_IT&query=${searchInput}`)
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                setMovies(res.results)
+            })
+
 
         fetch(`https://api.themoviedb.org/3/search/tv?api_key=${movieApiKey}&language=it_IT&query=${searchInput}`)
             .then((res) => res.json())
             .then((res) => {
                 console.log(res);
-                setSearchedMovies(res.results)
+                setTvSeries(res.results)
             })
+
 
     }
 
-    // useEffect(getSearchedItems, [])
+    function getSearchedItems() {
+
+        setSearched([...movies, ...tvSeries])
+    }
+
+    useEffect(getSearchedItems, [movies, tvSeries])
 
     return (
         <main>
@@ -30,22 +47,23 @@ export default function Main() {
                     <input value={searchInput} onChange={e => setSearchInput(e.target.value)}
                         type="text" className="form-control" placeholder="Search movies by title" aria-label="Search movies by title"
                         aria-describedby="button-addon2" />
-                    <button onClick={getSearchedItems}
+                    <button onClick={getMoviesAndTv}
                         className="btn btn-outline-secondary" type="button" id="button-addon2">
                         Search
                     </button>
                 </div>
 
                 <div className="row">
-                    {searchedMovies.map(movie => {
+                    {searched.map(movie => {
+
                         return (
                             <div key={movie.id} className="col-3">
                                 <div className="card">
                                     {/* <img className="card-img-top" src={`${movie.poster_path}`} alt="movie-img" /> */}
                                     <div className="card-body">
-                                        <h5 className="card-title">{movie.name}</h5>
-                                        <h5 className="card-title">{movie.original_name}</h5>
-                                        <p className="card-text">{movie.original_language}</p>
+                                        <h5 className="card-title">{movie.title}</h5>
+                                        <h6 className="card-title">{movie.original_title}</h6>
+                                        <img className="language-ico" src={flags[movie.original_language]} alt="" />
                                         <p className="card-text">{movie.vote_average}</p>
                                     </div>
                                 </div>
--- src/Contexts/MoviesProvider.jsx
+++ src/Contexts/MoviesProvider.jsx
@@ -1,22 +1,38 @@
 import { createContext, useContext, useState } from "react";
+import uk from '../assets/img/uk.png'
+import it from '../assets/img/it.png'
+import fr from '../assets/img/fr.png'
+import es from '../assets/img/es.png'
+import de from '../assets/img/de.png'
 
 const MoviesContext = createContext()
 
 function MoviesProvider({ children }) {
 
     const [movies, setMovies] = useState([])
-    const [searchedMovies, setSearchedMovies] = useState(movies)
+    const [tvSeries, setTvSeries] = useState([])
+    const [searched, setSearched] = useState([])
     const [searchInput, setSearchInput] = useState('')
 
+    //Create object with flag emoji for each language
+    const flags = {
+        en: uk,
+        it: it,
+        fr: fr,
+        es: es,
+        de: de,
+    }
 
     return (
         <MoviesContext.Provider
             value={{
                 movies, setMovies, searchInput, setSearchInput,
-                searchedMovies, setSearchedMovies
-            }}>
+                searched, setSearched, flags,
+                tvSeries, setTvSeries
+            }
+            }>
             {children}
-        </MoviesContext.Provider>
+        </MoviesContext.Provider >
     )
 }
 
